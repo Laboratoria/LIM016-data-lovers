7f75b3c5d7fed0f8dd5581125f53cd6f2c73cc92
nicolmendoza <nicolmendozamattos@gmail.com>
2021-10-30 14:43:11 -0500

    actualizacion nicol

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import data from "./data/pokemon/pokemon.js";
 
-console.log(data.pokemon[0]["pokemon-rarity"]);
-console.log(data.pokemon)
+console.log(data.pokemon[0].stats["max-cp"]);
 // console.log(data.pokemon[0].evolution["next-evolution"][0]["next-evolution"][0].name)
 // console.log(data.pokemon[0].type.includes('grass'));
 // console.log(data.pokemon[0].stats["max-hp"]);
@@ -12,42 +11,32 @@ const container = document.getElementById("root");
 const region = document.getElementById("region");
 const orden = document.getElementById("orden");
 const tipos = document.getElementById("tipos");
-const rareza=document.getElementById('rareza')
+const rareza = document.getElementById("rareza");
+const cp = document.getElementById("cp");
 
-const arrayPokemon=data.pokemon
-
-
-let arrayCambiante=""
-
+const arrayPokemon = data.pokemon;
+let arrayCambiante = "";
 
 //funcion que muestra los pokemones
 function mostrarPokemon(array) {
+  arrayCambiante = array;
 
-  arrayCambiante=array
+  const numero = document.getElementById("parrafo");
+  numero.innerHTML = `<p> Total :${array.length}</p>`;
+  for (let i = 0; i < array.length; i++) {
+    const parrafo = document.createElement("div");
+    const imagen = document.createElement("img");
+    const cpPokemon = document.createElement("p");
 
-  const numero=document.getElementById('parrafo')
-   numero.innerHTML=`<p> Total :${array.length}</p>`
-    for (let i = 0; i < array.length; i++) {
+    imagen.src = `${array[i].img}`;
+    parrafo.innerHTML = `<p>Nombre:${array[i].name}</p>`;
+    cpPokemon.innerHTML = `<p>CP: ${array[i].stats["max-cp"]}</p>`;
 
-
-      const parrafo = document.createElement("div");
-      const imagen = document.createElement("img");
-
-      imagen.src = `${array[i].img}`;
-
-      parrafo.innerHTML = `<p>Nombre:${array[i].name}</p>`;
-
-
-
-      document.getElementById("root").appendChild(parrafo);
-      document.getElementById("root").appendChild(imagen);
-    }
-
-
+    container.appendChild(parrafo);
+    container.appendChild(cpPokemon);
+    container.appendChild(imagen);
   }
-
-
-
+}
 
 //region pokemon
 region.addEventListener("change", (e) => {
@@ -66,13 +55,8 @@ function regionFuncion(valor) {
       array.push(arrayPokemon[i]);
     }
   }
-  mostrarPokemon(array)
-
+  mostrarPokemon(array);
 }
-
-
-
-
 
 //orden alfabetico
 orden.addEventListener("change", (e) => {
@@ -80,7 +64,6 @@ orden.addEventListener("change", (e) => {
   const typeOrden = e.target.value;
 
   if (typeOrden == "az") {
-
     arrayCambiante.sort((a, b) => {
       if (a.name < b.name) {
         return -1;
@@ -89,7 +72,6 @@ orden.addEventListener("change", (e) => {
     });
 
     mostrarPokemon(arrayCambiante);
-
   } else {
     arrayCambiante.sort((a, b) => {
       if (a.name > b.name) {
@@ -102,38 +84,53 @@ orden.addEventListener("change", (e) => {
   }
 });
 
-
-
 //tipos de pokemones
-
 tipos.addEventListener("change", (e) => {
   container.innerHTML = "";
   const tipoPokemon = e.target.value;
 
-
-
-// console.log(dataPoke[0].type.includes(tipoPokemon))
-  let tipos= arrayPokemon.filter(element=>
-
+  // console.log(dataPoke[0].type.includes(tipoPokemon))
+  let tipos = arrayPokemon.filter((element) =>
     element.type.includes(tipoPokemon)
+  );
 
-    )
-
-  //element>5
-console.log(tipos)
-mostrarPokemon(tipos)
+  console.log(tipos);
+  mostrarPokemon(tipos);
 });
 
-
-
 //pokemon rarity
-rareza.addEventListener('change',()=>{
+rareza.addEventListener("change", (e) => {
+  const tipoRareza = e.target.value;
+  container.innerHTML = "";
+  let arrayRareza = arrayPokemon.filter(
+    (array) => array["pokemon-rarity"] == tipoRareza
+  );
+  mostrarPokemon(arrayRareza);
 
+  console.log(tipoRareza);
+});
 
+//cp
+cp.addEventListener("change", (e) => {
+  container.innerHTML = "";
+  const tipoCp = e.target.value;
+  if (tipoCp == "max") {
+    arrayPokemon.sort((a, b) => {
+      if (Number(a.stats["max-cp"]) > Number(b.stats["max-cp"])) {
+        return -1;
+      }
+      return 1;
+    });
+  } else if (tipoCp == "min") {
+    arrayPokemon.sort((a, b) => {
+      if (Number(a.stats["max-cp"]) < Number(b.stats["max-cp"])) {
+        return -1;
+      }
+      return 1;
+    });
+  }
+
+  mostrarPokemon(arrayPokemon);
 })
 
-
-mostrarPokemon(arrayPokemon)
-
-
-//holii
+mostrarPokemon(arrayPokemon);
