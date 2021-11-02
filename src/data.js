150f555b9510e3fffd4a79802ba0050fa3bf741f
unknown <nataliabravojimenez@gmail.com>
2021-11-01 19:00:17 -0500

    ejemplo

--- src/data.js
+++ src/data.js
@@ -1,7 +1,12 @@
-// estas funciones son de ejemplo
+import data from './data/athletes/athletes.js';
+export const example = () =>{
 
-export const example = () => {
-  return 'example';
+  let example= Object.entries(data)[4][1]; //convierto al objeto en array, luego me quedé solo con el último elemento del array
+  //console.log(example);
+  
+  let equipos= example.map(example=> example.team);// me quedo solo con los equipos
+  //console.log(equipos);
+  return equipos;
 };
 
 export const anotherExample = () => {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { anotherExample, example } from './data.js'; //las funciones filter,etc
 
-console.log(example, data);
+import data from './data/athletes/athletes.js'; //la data del proyecto
+
+let info= data.athletes;  //me trae todos los atletas
+let exampl= example(info);
+//console.log(exampl);
+
+let pais= document.createElement("p"); //la caja donde se guardará los países
+  
+  pais.innerHTML= [...new Set(exampl)].sort().join("<p></p>");  //diciendole que información contendrá, se quita los dobles, se ordena alfabe
+  
+  document.getElementById("hereCountries").appendChild(pais);// indicandole el div donde se contendrá la información
