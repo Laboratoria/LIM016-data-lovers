052448b66c4d27c950e0684a33b1d26b63b40691
KarenBerrioRufino <karenberriorufino@gmail.com>
2021-11-04 18:45:06 -0500

    Adding my first changes

--- src/data.js
+++ src/data.js
@@ -1,9 +1,36 @@
+
 // estas funciones son de ejemplo
+/* export const example = () => {
+   return 'example';
+ };
+ export const anotherExample = () => {
+   return 'OMG';
+ };*/
 
-export const example = () => {
-  return 'example';
+export const functionAll = (datos) => {
+  const arr = datos.athletes;
+  const arr2 = [];
+  arr.filter(function (person) { //person es el nuevo objeto recorrido
+    console.table(person);
+    return person.age;
+    
+  });
+
+
+  //console.log(data.athletes, 'holi');
+
+  //FILTRAR POR EDAD
+  /*let ageFilter = data.filter(function(data){
+    return data.age===21;
+  })
+  console.log(ageFilter);*/
+
+
+  // let gender = athletes.filter(function(athletes){
+  // return athletes.gender === 'M';
+  // });
+  // console.log(gender);
+
+
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
