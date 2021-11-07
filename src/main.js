a9e1428ba6e4ffc36fe49dcf813cc65a129a8905
JenyferMarquez <favimarqez3@gmail.com>
2021-11-07 02:32:38 -0500

    obtener animaciones

--- src/main.js
+++ src/main.js
@@ -2,13 +2,28 @@
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 
-console.log(data.films[0].people["id"]);
+//console.log(data.films[0].people["id"]);
 
 fetch ('./data/ghibli/ghibli.json')
 .then(res => res.json() )
 .then( data=>{
     console.log(data.films)
     data.films.forEach(element=>{
-     console.log(element)
+        //console.log(element)
     })
 })
+.catch(error=> console.log(error))
+
+// asyn await  obtener peliculas
+
+const obtenerAnimaciones= async( ) => {
+try {
+ const res= await fetch ('./data/ghibli/ghibli.json')
+ const data= await res.json()
+     console.log(data.films)
+}catch (error){
+    console.log(error)
+}
+}
+
+obtenerAnimaciones();
