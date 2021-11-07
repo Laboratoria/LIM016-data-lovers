54e0a4fb2ee144615cbf92d99aee39cd99b9ceef
SharonNavarro <shanmowrk@gmail.com>
2021-11-07 18:54:04 -0500

    Agregamos un for para que recorra toda la data y que solo selecciona la propiedad que queremos. Tambien una funcion para manipular dinamicamente el DOM.

--- src/data.js
+++ src/data.js
@@ -1,7 +1,12 @@
 // estas funciones son de ejemplo
 
 export const example = () => {
+
   return 'example';
+};
+
+export const anotherExample = () => {
+  return 'OMG';
 };
 
 export const anotherExample = () => {
