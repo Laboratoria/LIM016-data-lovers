8cae6a0139c7e7127f415f5ca351f3ef00053b08
JenyferMarquez <favimarqez3@gmail.com>
2021-11-07 02:01:27 -0500

    cambios botones

--- src/main.js
+++ src/main.js
@@ -4,3 +4,11 @@ import data from './data/ghibli/ghibli.js';
 
 console.log(data.films[0].people["id"]);
 
+fetch ('./data/ghibli/ghibli.json')
+.then(res => res.json() )
+.then( data=>{
+    console.log(data.films)
+    data.films.forEach(element=>{
+     console.log(element)
+    })
+})
