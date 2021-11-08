73fae8f5cc90a853a0576daf3193d7ef3671bbb2
JenyferMarquez <favimarqez3@gmail.com>
2021-11-07 19:05:17 -0500

    cambio 1.1

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import data from './data/ghibli/ghibli.js';
 
 //console.log(data.films[0].people["id"]);
 
-fetch ('./data/ghibli/ghibli.json')
+/*fetch ('./data/ghibli/ghibli.json')
 .then(res => res.json() )
 .then( data=>{
     console.log(data.films)
@@ -12,7 +12,7 @@ fetch ('./data/ghibli/ghibli.json')
         //console.log(element)
     })
 })
-.catch(error=> console.log(error))
+.catch(error=> console.log(error))*/
 
 // asyn await  obtener peliculas
 
@@ -20,10 +20,15 @@ const obtenerAnimaciones= async( ) => {
 try {
  const res= await fetch ('./data/ghibli/ghibli.json')
  const data= await res.json()
+ const arrayTitle= data.films.map(anim => anim.title)
      console.log(data.films)
+const arrayFilm= data.films.filter(anim => anim.producer !="Isao Takahata")
+
+console.log(arrayFilm)
 }catch (error){
     console.log(error)
 }
 }
 
 obtenerAnimaciones();
+
