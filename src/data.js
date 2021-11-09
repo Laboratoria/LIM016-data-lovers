97713eb4d4e725f80f1b317731e462dd38dbfe91
Naimerith <naimerith89@gmail.com>
2021-11-08 20:24:55 -0500

    Filtre las medallas por pais

--- src/data.js
+++ src/data.js
@@ -1,44 +1,86 @@
-import data from './data/athletes/athletes';// el punto es para indicar que quiero buscar justo donde me encuentro
+//import athletes from "./data/athletes/athletes";
+
+ 
+ 
+  export const computeData = (datos, medal) =>{
+    const arrayOfTeam =[];
+    const deportes= datos.filter(athletes => athletes.medal === medal  )
+      for (let i= 0; i < deportes.length; i ++){
+        arrayOfTeam.push(deportes[i].team)
+      }
+      const teamOfSilver = [];
+      for (let j=0; j < arrayOfTeam.length; j++){
+        const totalSilverTeam = arrayOfTeam[j] + " " + arrayOfTeam.filter(team => team === arrayOfTeam[j]).length
+        teamOfSilver.push(totalSilverTeam)
+      }
+      const uniqueTeam = new Set (teamOfSilver)
+      console.log(uniqueTeam);
+   } 
+
+   
 
 
-export const sortData = (data, sortBy, sortOrder) =>{
+   
 
- return dataAthletes.sort(sortBy);
+   
 
-}
+  /* 
+  medalSilver: (medal, team) => {
+    const medalForTeam = athletes.filter(athletes => athletes.medal === "Silver")
+    for (i= 0; i < deportes.length; i ++){
+      array.push(deportes[i].team +" " + deportes[i].medal)
 
-// estas funciones son de ejemplo
-/* export const example = () => {
-   return 'example';
- };
- export const anotherExample = () => {
-   return 'OMG';
- };*/
+    }
+  }
+ }
+ console.log(objeto);
+ */
 
-export const functionAll = (datos) => {
-  const arr = datos.athletes;
-  const arr2 = [];
-  arr.filter(function (person) { //person es el nuevo objeto recorrido
-    console.table(person);
+
+
+  
+
+
+
+
+////ESTO ES DE LAS CHICAS -- NO TOCAR --  
+
+//import data from './data/athletes/athletes';// el punto es para indicar que quiero buscar justo donde me encuentro
+// export const sortData = (data, sortBy, sortOrder) =>{
+//   return dataAthletes.sort(sortBy);
+ 
+//  },
+ 
+ // estas funciones son de ejemplo
+ /* export const example = () => {
+    return 'example';
+  };
+  export const anotherExample = () => {
+    return 'OMG';
+  };*/
+ 
+  export const functionAll = (datos) => {
+   const arr = datos.athletes;
+    const arr2 = [];
+    arr.filter(function (person) { //person es el nuevo objeto recorrido
+      console.table(person);
+     });
     return person.age;
-    
-  });
 
-
-  //console.log(data.athletes, 'holi');
-
-  //FILTRAR POR EDAD
-  /*let ageFilter = data.filter(function(data){
-    return data.age===21;
-  })
-  console.log(ageFilter);*/
-
-
-  // let gender = athletes.filter(function(athletes){
-  // return athletes.gender === 'M';
-  // });
-  // console.log(gender);
-
-
-};
-
+ 
+    console.log(data.athletes, 'holi');
+ 
+    //FILTRAR POR EDAD
+   /*let ageFilter = data.filter(function(data){
+//      return data.age===21;
+//    })
+//    console.log(ageFilter);*/
+ 
+ 
+//    // let gender = athletes.filter(function(athletes){
+//    // return athletes.gender === 'M';
+//    // });
+//    // console.log(gender);
+ 
+ 
+ }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,57 @@
-import { sortData } from './data.js';
-import { functionAll } from './data.js';
-import datos from './data/athletes/athletes.js'; //jala de athletes.js
+import {computeData} from "./data.js";
+import datos from "./data/athletes/athletes.js";
 
+computeData(datos.athletes,"Gold") 
+computeData(datos.athletes,"Silver") 
+computeData(datos.athletes,"Bronze") 
+
+
+
+    /*  for (let i= 0; i < datos.athletes.length; i++) {
+    const filterTeam = datos.athletes[i].team;
+
+    console.log(filterTeam)
+    
+}  */
+/*const array =[];
+const deportes= datos.filter(athletes => athletes.medal === "Silver" )
+for (i= 0; i < deportes.length; i ++){
+ array.push(deportes[i].team +" " + deportes[i].medal)
+}
+console.log(array);*/
+
+
+// datos.filter(e=>e.athletes)
 // import data from './data/lol/lol.js';
 // import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //import datos from './data/athletes/athletes.js';
-console.log(functionAll(datos));
+//console.log(functionAll(datos));
 
-let printFilter = document.getElementById('printFilter');
+/*let printFilter = document.getElementById('printFilter');
 let arrFilter  = [];
-let checkboxes = document.querySelectorAll('.checkbox');
+let checkboxes = document.querySelectorAll('.checkbox');*/
+
+
+const open = document.getElementById("open");
+const modalContainer = document.getElementById("modal-container");
+const close = document.getElementById("close");
+
+
+open.addEventListener("click", () =>{
+    modalContainer.classList.add("show");
+});
+close.addEventListener("click", ()=>{
+    modalContainer.classList.remove("show");
+});
+
+
+
+
+
+
+
+
+
+
