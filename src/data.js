12e4ee3c4d3006ef3f6bde33d914d22ade34ab71
anakarina1928 <anakarina.nak@gmail.com>
2021-11-08 20:21:16 -0500

    orden de la data atletas

--- src/data.js
+++ src/data.js
@@ -1,19 +1,3 @@
-import data from './data/athletes/athletes';// el punto es para indicar que quiero buscar justo donde me encuentro
-
-
-export const sortData = (data, sortBy, sortOrder) =>{
-
- return dataAthletes.sort(sortBy);
-
-}
-
-// estas funciones son de ejemplo
-/* export const example = () => {
-   return 'example';
- };
- export const anotherExample = () => {
-   return 'OMG';
- };*/
 
 export const functionAll = (datos) => {
   const arr = datos.athletes;
@@ -42,3 +26,18 @@ export const functionAll = (datos) => {
 
 };
 
+/*exporto mi funcion para usarla en otro partes de la aplicacion*/ 
+// Este funcion recive 3 parametros, el primero es un Array de Athletas
+// El segundo parametro es una funcion de comparacion que va a utilizar el metodo sort, de Array para ordernar.
+// el tercer parametro se usara para ordenar de manera ascendente o descendente, este parametro es de tipo booleano: 
+// Si es true entonces el ordenamiento sera ascendente, si es false entonces sera descendente
+export const sortData = (data, sortBy, sortOrder) =>{
+    
+    if (sortOrder) {
+        return data.sort(sortBy);/*con el metodo sort hago una ordenacion alfabetica. Con sortBy (funcion 
+            de comparacion) ordenara bajo un criterio*/
+    } else {
+        return data.sort(sortBy).reverse();
+    }
+   
+   }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,124 @@
 import { sortData } from './data.js';
 import { functionAll } from './data.js';
-import datos from './data/athletes/athletes.js'; //jala de athletes.js
-
-// import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-//import datos from './data/athletes/athletes.js';
-console.log(functionAll(datos));
+import datos from './data/athletes/athletes.js';
 
 let printFilter = document.getElementById('printFilter');
 let arrFilter  = [];
 let checkboxes = document.querySelectorAll('.checkbox');
+///////////////////////////////////////////////////////////////////////////////////////
+
+/*
+const sortByCountry = (athlete1,athlete2) => {
+
+    if (athlete1.team > athlete2.team) {
+        return 1;
+      }
+      if (athlete1.team < athlete2.team) {
+        return -1;
+      }
+      return 0;
+};
+ 
+const htmlAthletes = sortData(arrayAthletes, sortByName,).map(generateAthleteTemplate).join('');
+
+*/
+
+
+const arrayAthletes = datos.athletes;//aqui guardo la data de todos los atletas
+
+const generateAthleteTemplate = (athlete) => {/* una funcion  que retorna con la plantilla de un
+     solo atleta que es un string (es un html)*/  
+    return `<article class="sportsContainer">
+    <section class="boxImgAthlete">
+        <img class="classAthlete" src="imagenes/deportista.PNG">
+    </section>
+    <section class="tableAthletes">
+        <p>&#128308 Nombre:${athlete.name}</p>
+        <p>&#128308 Genero:${athlete.gender}</p>
+        <p>&#128308 Altura:${athlete.height}</p>
+        <p>&#128308 Peso:${athlete.weight}</p>
+        <p>&#128308 Pais:${athlete.team}</p>
+        <p>&#128308 Edad:${athlete.age}</p>
+        <p>&#128308 Medalla:${athlete.medal}</p>
+    </section>
+  </article>`
+}
+
+//insertHtmlAtheles(htmlAthletes);//utilzo como argumento mi nuevo arreglo que converti en string.
+
+const optionAge = document.getElementById('optionAge');
+const nameOpward = document.getElementById('nameOpward');
+const nameFallig = document.getElementById('nameFallig');
+
+const insertHtmlAtheles = (htmlAthletes) => { /*funcion para insertar los atletas en mi web.
+    En mi id"gridForTest" inserto con innerHTML.*/
+
+   const grid = document.getElementById("gridForTest");
+   grid.innerHTML = htmlAthletes;
+}
+
+
+const sortByName = (athlete1,athlete2)=>{
+
+    if (athlete1.name > athlete2.name) {
+        return 1;
+      }
+      if (athlete1.name < athlete2.name) {
+        return -1;
+      }
+      return 0;
+};
+
+const sortByAge = (athlete1,athlete2) => {
+
+    if (athlete1.age > athlete2.age) {
+        return 1;
+      }
+      if (athlete1.age < athlete2.age) {
+        return -1;
+      }
+      return 0;
+};
+/*utilizo map para obtener un nuevo arreglo. Su objetivo es devolver un 
+nuevo arreglo donde cada uno de sus elementos será lo que devuelva la función generateAthleteTemplate 
+(como un ciclo).
+ 
+Con "join" convierto ese nuevo arreglo en un string y asi lo concateno.*/
+
+document.getElementById("orderBySelect").addEventListener('change', (event) => {
+    const sortByValue = event.target.value;// investigar de esto
+    if (sortByValue === "1") {
+        const htmlAthletesAge = sortData(arrayAthletes, sortByAge,true).map(generateAthleteTemplate).join('');
+       insertHtmlAtheles(htmlAthletesAge);//utilzo como argumento mi nuevo arreglo que converti en string.
+        
+    }
+    
+    if (sortByValue ==="2"){
+        const htmlAthletesName= sortData(arrayAthletes, sortByName,true).map(generateAthleteTemplate).join('');
+        insertHtmlAtheles(htmlAthletesName);
+    }
+
+    if (sortByValue ==="3"){
+        const htmlAthletesNameReverse= sortData(arrayAthletes, sortByName,false).map(generateAthleteTemplate).join('');
+          insertHtmlAtheles(htmlAthletesNameReverse);
+        
+    }
+});
+
+// optionAge.addEventListener('click', () =>{
+//      const htmlAthletesAge = sortData(arrayAthletes, sortByAge,true).map(generateAthleteTemplate).join('');
+//      insertHtmlAtheles(htmlAthletesAge);//utilzo como argumento mi nuevo arreglo que converti en string.
+// });
+
+// nameOpward.addEventListener('click', () =>{
+//     const htmlAthletesName= sortData(arrayAthletes, sortByName,true).map(generateAthleteTemplate).join('');
+//     insertHtmlAtheles(htmlAthletesName);
+
+// });
+
+// nameFallig.addEventListener('click', () =>{
+//     const htmlAthletesNameReverse= sortData(arrayAthletes, sortByName,false).map(generateAthleteTemplate).join('');
+//     insertHtmlAtheles(htmlAthletesNameReverse);
+
+// });
+
