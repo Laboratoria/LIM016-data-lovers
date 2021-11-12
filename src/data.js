2cc2cb787b841d5702b0114317201a859f449b15
JocelynCamero <jocelyncamero@gmail.com>
2021-11-12 10:23:06 -0500

    Buscador sin limpiar

--- src/data.js
+++ src/data.js
@@ -1,10 +1,28 @@
-
-
-// estas funciones son de ejemplo
-export const example = () => {
-  return pokemon;
+export const mostraPokemon = (pokemon, fragment, template, container) => {
+  for(let i=0;i<pokemon.length;i++){
+  template.content.querySelector(".numPokemon").textContent=pokemon[i].num;
+  template.content.querySelector(".imgPokemon").src=pokemon[i].img;
+  template.content.querySelector(".nomPokemon").textContent=pokemon[i].name;
+  template.content.querySelector(".nomRegion").textContent=pokemon[i].generation.name;
+  
+  const clone=template.content.cloneNode(true);
+  fragment.appendChild(clone); 
+  }
+  container.appendChild(fragment);
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const buscarPokemon = (pokemon, input, fragment, template,container) => {
+  
+  for(let i=0;i<pokemon.length;i++){
+  if(pokemon[i].name.includes(input)){
+      template.content.querySelector(".numPokemon").textContent=pokemon[i].num;
+      template.content.querySelector(".imgPokemon").src=pokemon[i].img;
+      template.content.querySelector(".nomPokemon").textContent=pokemon[i].name;
+      template.content.querySelector(".nomRegion").textContent=pokemon[i].generation.name;
+      
+      const clone=template.content.cloneNode(true);
+      fragment.appendChild(clone); 
+      }
+      }
+      container.appendChild(fragment);
 };
