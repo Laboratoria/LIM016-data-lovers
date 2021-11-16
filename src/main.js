39f4022981fa065d919820871840ae89e8f8ad3f
kalyzca <alcantarakaly@gmail.com>
2021-11-16 17:06:45 -0500

    primer cambio

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+let parrafoData = document.getElementById("data-personaje")
+//obteniendo la data del objeto results de rickyandmorty.js
+parrafoData.textContent=data.results[0].name;
