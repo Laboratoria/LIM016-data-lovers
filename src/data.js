18dc545b7438ee5f99ebf075195ee283aa13a447
anakarina1928 <anakarina.nak@gmail.com>
2021-11-22 09:55:34 -0500

    select de atletas y filtrado

--- src/data.js
+++ src/data.js
@@ -1,44 +1,99 @@
 
+//funcion para filtrar
+const functionAll = (array, condicion) => {
+  return array.filter(condicion);
+};
 
-export const functionAll = (datos) => {
-  const arrayAthletes = datos.athletes;
-  //const arr2 = [];
-  arrayAthletes.filter(function (person) { //person es el nuevo objeto recorrido
-    //console.table(person, 'hola'); //para filtrar toda la tabla de datos
-    console.log(person.team, 'holi'); //para filtrar por categorias
-    return person.age;
-    
+
+/* funcion para obtener un arreglo de solo paises y deportes*/
+const allCountries = (countries) => {
+  const newArrCountries = countries.map(newTeam => {
+    return newTeam.team
   });
+  return new Set(newArrCountries);
+}
+
+const allSport = (sport) => {
+  const newArrSport = sport.map(newTeam => {
+    return newTeam.sport
+  });
+  return new Set(newArrSport);
+}
+
+const allTeams = (athletes) => {
+  const allTeamsWithDuplicates = athletes.map(person => {
+    return person.team
+  });
+  return new Set(allTeamsWithDuplicates);
+}
+
+/*
+ Este funcion recive 3 parametros, el primero es un Array de Athletas
+ El segundo parametro es una funcion de comparacion que va a utilizar el metodo sort, de Array para ordernar.
+ el tercer parametro se usara para ordenar de manera ascendente o descendente, este parametro es de tipo booleano: 
+ Si es true entonces el ordenamiento sera ascendente, si es false entonces sera descendente
+*/
+const sortData = (data, sortBy, sortOrder) => {
+  if (sortOrder) {//true
+    return data.sort(sortBy);/*con el metodo sort hago una ordenacion alfabetica. Con sortBy (funcion 
+            de comparacion) ordenara bajo un mi criterio*/
+  } else {//false
+    return data.sort(sortBy).reverse();
+  }
 
 }
-/*exporto mi funcion para usarla en otro partes de la aplicacion*/ 
-// Este funcion recive 3 parametros, el primero es un Array de Athletas
-// El segundo parametro es una funcion de comparacion que va a utilizar el metodo sort, de Array para ordernar.
-// el tercer parametro se usara para ordenar de manera ascendente o descendente, este parametro es de tipo booleano: 
-// Si es true entonces el ordenamiento sera ascendente, si es false entonces sera descendente
+/*mis funciones de comparacion para utilizarlas en mi funcion sort y poder ordenar de una forma
+mas natural (1,2,3,4 o a,b,c,d) ya que con sort los elementos si se ordenan pero de una forma predeterminada
+del navegador y no es una ordenacion que deseo*/
 
-export const sortData = (data, sortBy, sortOrder) =>{
-    
-    if (sortOrder) {
-        return data.sort(sortBy);/*con el metodo sort hago una ordenacion alfabetica. Con sortBy (funcion 
-            de comparacion) ordenara bajo un criterio*/
-    } else {
-        return data.sort(sortBy).reverse();
-    }
-   
-   }
+const sortByName = (athlete1, athlete2) => {//ordeno atletas
+  if (athlete1.name > athlete2.name) {
+    /*si el primer elemento es mayor se colocara en una posicion
+     despues del segundo*/
+    return 1;
+  }
+  if (athlete1.name < athlete2.name) {
+    return -1;
+    /*si el lemento es menos que el segundo se resta y se coloca 
+    en una posicion menor al segundo.*/
+  }
+  return 0;
+  /* si los dos elementos son iguales se quedaran en la misma posicion*/
+};
 
-  export const computeData = (datos, medal) =>{
-    const arrayOfTeam =[];
-    const deportes= datos.filter(athletes => athletes.medal === medal  )
-      for (let i= 0; i < deportes.length; i ++){
-        arrayOfTeam.push(deportes[i].team)
-      }
-      const teamOfSilver = [];
-      for (let j=0; j < arrayOfTeam.length; j++){
-        const totalSilverTeam = arrayOfTeam[j] + " " + arrayOfTeam.filter(team => team === arrayOfTeam[j]).length
-        teamOfSilver.push(totalSilverTeam)
-      }
-      const uniqueTeam = new Set (teamOfSilver)
-      console.log(uniqueTeam);
-   }
+const sortByAge = (athlete1, athlete2) => {//ordeno por edad
+  if (athlete1.age > athlete2.age) {
+    return 1;
+  }
+  if (athlete1.age < athlete2.age) {
+    return -1;
+  }
+  return 0;
+};
+
+//funcion para el medallero
+const computeData = (datos, medal) => {
+  const arrayOfTeam = [];
+  const deportes = datos.filter(athletes => athletes.medal === medal)
+  for (let i = 0; i < deportes.length; i++) {
+    arrayOfTeam.push(deportes[i].team)
+  }
+  const teamOfSilver = [];
+  for (let j = 0; j < arrayOfTeam.length; j++) {
+    const totalSilverTeam = arrayOfTeam[j] + " " + arrayOfTeam.filter(team => team === arrayOfTeam[j]).length
+    teamOfSilver.push(totalSilverTeam)
+  }
+  const uniqueTeam = new Set(teamOfSilver)
+  console.log(uniqueTeam);
+}
+
+export {
+  functionAll,
+  allCountries,
+  allSport,
+  allTeams,
+  sortData,
+  sortByName,
+  sortByAge,
+  computeData,
+}
--- src/main.js
+++ src/main.js
@@ -1,32 +1,27 @@
 import datos from "./data/athletes/athletes.js";
-import {computeData} from "./data.js";
-import { sortData } from './data.js';
-import { functionAll } from './data.js';
-
-
-console.log(functionAll(datos));
-//----------------SELECTOR DINÁMICO PARA EL FILTRO DE PAÍSES----------------
-//Para esconder y mostrar las opciones
-const select = document.querySelector('.select');
-select.addEventListener('click', function(){
-    document.getElementById('options').classList.toggle('active');
-});
-//Para que el contenido de las opciones se muestreen la parte superior
-//const contentSelect = document.querySelector('.contentSelect');
-//const inputSelect = document.querySelector('#inputSelect');
+import { computeData, functionAll, sortData, sortByAge, sortByName, allCountries, allSport } from "./data.js";
 
 const arrayAthletes = datos.athletes;//aqui guardo la data de todos los atletas
+const arrayCountries = sortData(Array.from(allCountries(arrayAthletes))).reverse();//array de paises
+const arraySport = sortData(Array.from(allSport(arrayAthletes))).reverse();// array de dportes
+const team = document.getElementById('team');//select de paises
+const sport = document.getElementById('sport');//select de dorpote
+const orderBySelect = document.getElementById('orderBySelect');//select para ordenar 
 
-const generateAthleteTemplate = (athlete) => {/* una funcion  que retorna con la plantilla de un
-     solo atleta que es un string (es un html)*/  
-    return `<article class="sportsContainer">
+
+// una funcion  que retorna con la plantilla de un solo atleta que es un string (es un html)
+const generateAthleteTemplate = (athlete) => {
+
+  const athletegender = athlete.gender === "F" ? "./imagenes/atletafemenino2.jpg" : "./imagenes/atletasmasculinos.jpg"
+  return `<article class="sportsContainer">
     <section class="boxImgAthlete">
-        <img class="classAthlete" src="imagenes/deportista.PNG">
+        <img class="classAthlete" src= ${athletegender}>
     </section>
     <section class="tableAthletes">
-        <p>&#128308 Nombre:${athlete.name}</p>
+        <p class="nameAthlete">Nombre:${athlete.name}</p>
         <p>&#128308 Genero:${athlete.gender}</p>
         <p>&#128308 Altura:${athlete.height}</p>
+        <p>&#128308 Deporte:${athlete.sport}</p>
         <p>&#128308 Peso:${athlete.weight}</p>
         <p>&#128308 Pais:${athlete.team}</p>
         <p>&#128308 Edad:${athlete.age}</p>
@@ -34,99 +29,126 @@ const generateAthleteTemplate = (athlete) => {/* una funcion  que retorna con la
     </section>
   </article>`
 }
+// funcion que retorna plantilla de  optiones de los filtros
+const generateOptionTemplate = (arrayF) => {
 
-const optionAge = document.getElementById('optionAge');
-const nameOpward = document.getElementById('nameOpward');
-const nameFallig = document.getElementById('nameFallig');
+  return `<option value="${arrayF}"> ${arrayF} </option> `
+}
+// variable que tiene al opcion predeterminada de los select
+const OptionselectedTemplate = `<option value="todos" selected="selected"> Todos </option>`;
 
-const insertHtmlAtheles = (htmlAthletes) => { /*funcion para insertar los atletas en mi web.
-    En mi id"gridForTest" inserto con innerHTML.*/
+//funcion para insertar las opciones de los dos select en html
+const insertHtmArray = (elemet, htmlArray) => {
 
-   const grid = document.getElementById("gridForTest");
-   grid.innerHTML = htmlAthletes;
+  elemet.innerHTML = OptionselectedTemplate + htmlArray;/*aqui voy a insertar mi plantilla que esta
+ en generateOptionTemplate*/
 }
 
+//funcion para insertar los atletas en mi web.En mi id"gridForTest" inserto con innerHTML. 
+const insertHtmlAtheles = (htmlAthletes) => {
 
-const sortByName = (athlete1,athlete2)=>{
+  const grid = document.getElementById("gridForTest");
+  grid.innerHTML = htmlAthletes;/*aqui voy a insertar mi plantilla que esta
+   en generateAthleteTemplate*/
+}
 
-    if (athlete1.name > athlete2.name) {
-        return 1;
-      }
-      if (athlete1.name < athlete2.name) {
-        return -1;
-      }
-      return 0;
+//aqui genero la lista de opciones para el filtro de paises y deportes.
+let htmlCountrie = arrayCountries.map(generateOptionTemplate).join("");
+insertHtmArray(team, htmlCountrie);
+let htmlSport = arraySport.map(generateOptionTemplate).join("");
+insertHtmArray(sport, htmlSport);
+
+//declaro estas dos funciones afuera porque necesito usarlas en los dos addEventListener
+const filterByTeamFunc = (teamSelected) => {/*funcion que me retorna una funcion los atletas de un pais 
+  que sean igual al valor del select */
+  return (athlete) => athlete.team == teamSelected;
+}
+
+const filterBySportFunc = (sportSelected) => {/*funcion que me retorna una funcion de atletas por deportes 
+  que sean igual al valor del select */
+  return (athlete) => athlete.sport == sportSelected;
+}
+let filtersToSort = [];/*esta varieble la utilizo para guardar todos los filtros realizados y 
+utilizarlos para ordenar.*/
+
+//funcion que trabaja con todos los filtros.
+const functionFilterGrouping = () => {
+
+  const sportSelected = sport.value;//*guardo el valor(la accion del usuario)
+  const teamSelected = team.value;//*guardo el valor(la accion del usuario)
+
+  /* uso condicionales para comparar: si el valor de mis dos select es igual igual a "todos" se mostrara 
+   la data completa sin filtrar  y con un map nos debuelve el arreglo modificado con la platilla de los atletas */
+  if (teamSelected === "todos" && sportSelected === "todos") {
+
+    insertHtmlAtheles(arrayAthletes.map(generateAthleteTemplate).join(''));
+    filtersToSort = arrayAthletes;
+
+  }
+  if (teamSelected !== "todos" && sportSelected === "todos") {
+    let filterOnlyByCountry = functionAll(arrayAthletes, filterByTeamFunc(teamSelected));
+    filtersToSort = filterOnlyByCountry;
+    insertHtmlAtheles(filterOnlyByCountry.map(generateAthleteTemplate).join(''));
+
+  }
+
+  /*pero si el usuario selecciona la opcion todos en el filtro "pais" y selecciona un deporte que no es igual "todos"
+  utilizo mi funcion para filtrar donde le paso como argumento la data de los altetas y el otro argumento seria mi 
+  condicion que es la funcion que me retorna una funcion que filtra  atletas por pais que sean igual al valor del select */
+  if (teamSelected === "todos" && sportSelected !== "todos") {
+    let filteredAthletes = functionAll(arrayAthletes, filterBySportFunc(sportSelected));
+    filtersToSort = filteredAthletes;
+    insertHtmlAtheles(filteredAthletes.map(generateAthleteTemplate).join(''));
+
+  }
+  if (teamSelected !== "todos" && sportSelected !== "todos") {
+    //primero hago el filtro de atletas por pais que sean igual al valor del select
+    const filteredByTeam = functionAll(arrayAthletes, filterByTeamFunc(teamSelected));
+    /*despues ese nuevo arreglo filtrado lo vuelvo a filtrar  y le coloco como condicion mi funcion que filtra atletas
+    por deporte que sea igual al valor seleccionado por el usuario.*/
+    const filteredByTeamAndSport = functionAll(filteredByTeam, filterBySportFunc(sportSelected));
+    filtersToSort = filteredByTeamAndSport;
+    insertHtmlAtheles(filteredByTeamAndSport.map(generateAthleteTemplate).join(''));
+
+  }
+  //console.log("containerFatherMain: ", document.getElementById('containerFatherMain'));
+  document.getElementById('imgBetterWord').style.display = 'none';//ocultamos
+  document.getElementById('carousel').style.display = 'none';//ocultamos
+  //document.getElementById('displaySport').style.display = 'block';//mostramos
+  document.getElementById('displayOrder').style.display = 'block';//mostramos
+
 };
 
-const sortByAge = (athlete1,athlete2) => {
 
-    if (athlete1.age > athlete2.age) {
-        return 1;
-      }
-      if (athlete1.age < athlete2.age) {
-        return -1;
-      }
-      return 0;
-};
-/*utilizo map para obtener un nuevo arreglo. Su objetivo es devolver un 
-nuevo arreglo donde cada uno de sus elementos será lo que devuelva la función generateAthleteTemplate 
-(como un ciclo).
- 
-Con "join" convierto ese nuevo arreglo en un string y asi lo concateno.*/
+team.addEventListener('change', functionFilterGrouping);
+sport.addEventListener('change', functionFilterGrouping);
 
-document.getElementById("orderBySelect").addEventListener('change', (event) => {
-    const sortByValue = event.target.value;// investigar de esto
-    if (sortByValue === "1") {
-        const htmlAthletesAge = sortData(arrayAthletes, sortByAge,true).map(generateAthleteTemplate).join('');
-       insertHtmlAtheles(htmlAthletesAge);//utilzo como argumento mi nuevo arreglo que converti en string.
-        
-    }
-    
-    if (sortByValue ==="2"){
-        const htmlAthletesName= sortData(arrayAthletes, sortByName,true).map(generateAthleteTemplate).join('');
-        insertHtmlAtheles(htmlAthletesName);
-    }
 
-    if (sortByValue ==="3"){
-        const htmlAthletesNameReverse= sortData(arrayAthletes, sortByName,false).map(generateAthleteTemplate).join('');
-          insertHtmlAtheles(htmlAthletesNameReverse);
-        
-    }
+//orderBySelect es donde se encuentra el select de mis opciones para ordenar.
+orderBySelect.addEventListener('change', (event) => {
+  const sortByValue = event.target.value;/*guardo el eveto(la accion del usuario), su target y el valor que le puse*/
+  if (sortByValue === "1") { //lo comparo
+    //Aqui utilizo mi funcion sortData para ordenar mi arreglo original "arrayAthletes", le paso mi 
+    //funcion de comparacion "sorByAge" y le indico la ordenacion que es ascendente con "true"
+
+    let athleteSortByAge = sortData(filtersToSort, sortByAge, true);/*utilizo como argumento mi nuevo 
+    arreglo que converti en string.*/
+    insertHtmlAtheles(athleteSortByAge.map(generateAthleteTemplate).join(''));
+  }
+
+  if (sortByValue === "2") {
+    let athleteSortByName = sortData(filtersToSort, sortByName, true);
+    insertHtmlAtheles(athleteSortByName.map(generateAthleteTemplate).join(''));
+  }
+
+  if (sortByValue === "3") {
+    let sortByNameReverse = sortData(filtersToSort, sortByName, false);
+    insertHtmlAtheles(sortByNameReverse.map(generateAthleteTemplate).join(''));
+  }
 });
 
-
-/*
-const sortByCountry = (athlete1,athlete2) => {
-
-    if (athlete1.team > athlete2.team) {
-        return 1;
-      }
-      if (athlete1.team < athlete2.team) {
-        return -1;
-      }
-      return 0;
-};
- 
-const htmlAthletes = sortData(arrayAthletes, sortByName,).map(generateAthleteTemplate).join('');
-////////////////////////////////////
-
-*/
-computeData(datos.athletes,"Gold") 
-computeData(datos.athletes,"Silver") 
-computeData(datos.athletes,"Bronze") 
-
-/*
-const open = document.getElementById("open");
-const modalContainer = document.getElementById("modal-container");
-const close = document.getElementById("close");
-
-
-open.addEventListener("click", () =>{
-    modalContainer.classList.add("show");
-});
-close.addEventListener("click", ()=>{
-    modalContainer.classList.remove("show");
-});
-*/
-
+//Nai
+computeData(datos.athletes, "Gold")
+computeData(datos.athletes, "Silver")
+computeData(datos.athletes, "Bronze")
 
