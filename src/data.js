1eeb22944273c2278c6aad05797a1ac92ef173a3
SharonNavarro <shanmowrk@gmail.com>
2021-11-25 11:53:05 -0500

    recibiendo cambios

--- src/data.js
+++ src/data.js
@@ -80,22 +80,6 @@ export const ordenNumericoAscL = (array,parametro,llave) => {
   }
 }
 
-export const ordenNumericoAscL = (array,parametro,llave) => {
-  let sortArray = [];
-  for(let i=0; i<array.length;i++){
-    sortArray.push(array[i][llave])
-  }
-
-  if ((!sortArray.includes(Number))) {
-  const compareNumber = sortArray.flat(1).sort((a, b) => {
-    return b[parametro] - a[parametro];
-  });
-  return compareNumber
-  }
-}
-
-
-
 
 
 
