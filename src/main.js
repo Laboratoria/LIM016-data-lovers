c9fa6a323db8754a5938768ab91ff019d14c787f
JenyferMarquez <favimarqez3@gmail.com>
2021-11-28 21:40:26 -0500

    trayendo cambios

--- src/main.js
+++ src/main.js
@@ -426,50 +426,6 @@ document.getElementById("cleanVehicles").addEventListener("click",function() {
     
 
 
-/*function statisticsDirector(ctx) {
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels:["Yoshifumi Kondō", "Isao Takahata", "Hiromasa Yonebayashi", "Gorō Miyazaki",  "Hiroyuki Morita", "Hayao Miyazaki" ],
-      datasets: [
-        {
-          label: "Número de Películas Dirigidas",
-          data: [1,5,2,2,1,9],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-           
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-         
-          ],
-          borderWidth: 1
-        }
-      ]
-    },
-    options:{
-      indexAxis: 'x',
-    }   
-  })
-}
-// Llamando desde el DOM
-function mostrarCharts() {
-  const ctx = document.getElementById('myChart').getContext('2d');
-  statisticsDirector(ctx)
-}
-
-mostrarCharts();*/
-
 
 function statisticsScore(ctx) {
  // eslint-disable-next-line no-undef
