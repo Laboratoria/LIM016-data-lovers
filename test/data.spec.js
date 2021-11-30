80f2e64894aacbc40d4ba850ed115b4713c17221
JenyferMarquez <favimarqez3@gmail.com>
2021-11-30 07:53:44 -0500

    main

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,17 +1,28 @@
-import { sortOrdenAlfabeticoAsc, anotherExample } from '../src/data.js';
+import { sortOrdenNumericoAsc} from '../src/data.js';
 
 
-describe('sortOrdenAlfabeticoAsc', () => {
-  it('is a function ', () => {
-    expect(typeof sortOrdenAlfabeticoAsc).toBe('function');
+describe('sort ordena score numerico en forma ascendente', () => {
+  it('is a function sortOrdenNumericoAsc', () => {
+    expect(typeof sortOrdenNumericoAsc).toBe('function');
   });
 
 
 
-  it('returns `a-z`', () => {
+  /*it('returns ``', () => {
     const compareTitle = [{"title": "Castle in the Sky"},{"title": "Whisper of the Heart"}]
-    expect(sortOrdenAlfabeticoAsc(compareTitle)).toEqual('example');
-  }); 
+    expect(sortOrdenAlfabeticoAsc(compareTitle)).toEqual([{"title": "Castle in the Sky"},{"title": "Whisper of the Heart"}]);
+  }); */
+});
+
+
+describe('searchData', () => {
+  it('is a function', () => {
+    expect(typeof searchData).toBe('function');
+  });
+  const dataPrueba = [{title: "Whisper of the Heart",rt_score:85,release_date:1995}, {  title: "Castle in the Sky", rt_score:100, release_date:2000}];
+  it('should return the search result title',()=>{
+    expect(searchData(dataPrueba,`title`,`whisper`)).toEqual([{"title": "Whisper of the Heart","rt_score":85,"release_date":1995}])
+  })
 });
 
 
@@ -19,7 +30,6 @@ describe('anotherExample', () => {
   it('is a function', () => {
     expect(typeof anotherExample).toBe('function');
   });
-
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
   });
