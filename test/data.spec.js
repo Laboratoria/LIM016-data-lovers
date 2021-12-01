adcdd656efea414b02badbd6d1f5f96c00450570
SharonNavarro <shanmowrk@gmail.com>
2021-12-01 01:00:59 -0500

    Revision y edicion de codigo en data.js y main.js. Todo funcional. Actualizacion en progreso para responsive, no terminado

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,27 @@
-import { example, anotherExample } from '../src/data.js';
+import {filterItems, sortOrdenAlfabeticoAsc, sortOrdenAlfabeticoDesc, filterData, sortOrdenNumericoAsc, sortOrdenNumericoDesc, filterPeopleLocationsVehicles, filterAge, inputSearch} from '../src/data.js';
+
+const filterItems = require('./filterItems')
+
+describe('filterItems', () => {
+  it('is a function', () => {
+    expect(typeof filterItems).toBe('function');
+  });
+
+  it('returns `filterItems`', () => {
+    expect(filterItems()).toBe('example');
+  });
+});
+
+
+describe('anotherExample', () => {
+  it('is a function', () => {
+    expect(typeof anotherExample).toBe('function');
+  });
+
+  it('returns `anotherExample`', () => {
+    expect(anotherExample()).toBe('OMG');
+  });
+});
 
 
 describe('example', () => {
@@ -12,12 +35,88 @@ describe('example', () => {
 });
 
 
-describe('anotherExample', () => {
+describe('example', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof example).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });
+});
+
+
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });
+});
+
+
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });
+});
+
+
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });
+});
+
+
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });
+});
+
+
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });
+});
+
+
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });
+});
+
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
   });
 });
